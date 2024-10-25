60e2ab46d71e3fccc4de3d1834a8dfe60d1a2e3d
haydaramru <haydaramrurevanda@mail.ugm.ac.id>
2024-10-26 00:09:02 +0700

    added a whole lot of stuff (again).

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,104 +12,202 @@ import TekananUdara from "../components/TekananUdara";
 import MapWrapper from "../components/Map";
 
 const Dashboard = () => {
-  // const [kecepatanAnginData, setKecepatanAnginData] = useState(0);
-  // const generateRandomKecepatanAnginData = () => {
-  //   return (Math.random() * 10).toFixed(2);
-  // };
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     const newKecepatanAngin = generateRandomKecepatanAnginData();
-  //     setKecepatanAnginData(newKecepatanAngin);
-  //   }, 1000);
-
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
-
-  // const [ketinggianData, setKetinggianData] = useState(0);
-  // const generateRandomKetinggianData = () => {
-  //   return (Math.random() * 10).toFixed(2); 
-  // };
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     const newKetinggian = generateRandomKetinggianData();
-  //     setKetinggianData(newKetinggian);
-  //   }, 1000);
-
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
-
-  // const [kelembabanData, setKelembabanData] = useState(0);
-  // const generateRandomKelembabanData = () => {
-  //   return (Math.random() * 10).toFixed(2);
-  // };
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     const newKelembaban = generateRandomKelembabanData();
-  //     setKelembabanData(newKelembaban);
-  //   }, 1000);
-
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
-
-  // const [curahHujanData, setCurahHujanData] = useState(0);
-  // const generateRandomCurahHujan = () => {
-  //   return (Math.random() * 10).toFixed(2);
-  // };
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     const newCurahHujan = generateRandomCurahHujan();
-  //     setCurahHujanData(newCurahHujan);
-  //   }, 1000);
-
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
-
-  // const [tekananUdaraData, setTekananUdaraData] = useState(0);
-  // const generateRandomTekananUdara = () => {
-  //   return (Math.random() * 10).toFixed(2);
-  // };
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     const newTekananUdara = generateRandomTekananUdara();
-  //     setTekananUdaraData(newTekananUdara);
-  //   }, 1000);
-
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
-
-
-  const [socketData, setSocketData] = useState({});
-
+  const [kecepatanAnginData, setKecepatanAnginData] = useState(0);
+  const generateRandomKecepatanAnginData = () => {
+    return (Math.random() * (5.5 - 4) + 4).toFixed(1);
+  };
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("Connected to Socket.IO server");
-    });
+    const intervalId = setInterval(() => {
+      const newKecepatanAngin = generateRandomKecepatanAnginData();
+      setKecepatanAnginData(newKecepatanAngin);
+    }, 1000);
 
-    socket.on("message", (mess) => {
-      const dataArray = mess.split(",");
-      const data = {
-        distance:       parseFloat(dataArray[0]),
-        lightIntensity: parseFloat(dataArray[1]),
-        uvIndex:        parseFloat(dataArray[2]),
-        temperature:    parseFloat(dataArray[3]),
-        humidity:       parseFloat(dataArray[4]),
-        windSpeed:      parseFloat(dataArray[5]),
-        gustSpeed:      parseFloat(dataArray[6]),
-        windDirection:  parseFloat(dataArray[7]),
-        rainfall:       parseFloat(dataArray[8]),
-        absPressure:    parseFloat(dataArray[9]),
-      }
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [ketinggianData, setKetinggianData] = useState(0);
+  const generateRandomKetinggianData = () => {
+    return (Math.random() * (1980 - 1940) + 1940).toFixed(1); 
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newKetinggian = generateRandomKetinggianData();
+      setKetinggianData(newKetinggian);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [kelembabanData, setKelembabanData] = useState(0);
+  const generateRandomKelembabanData = () => {
+    return Math.floor(Math.random() * (65 - 60) + 60);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newKelembaban = generateRandomKelembabanData();
+      setKelembabanData(newKelembaban);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [curahHujanData, setCurahHujanData] = useState(0);
+  const generateRandomCurahHujan = () => {
+    return (Math.random() * (19 - 18) + 18).toFixed(1);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newCurahHujan = generateRandomCurahHujan();
+      setCurahHujanData(newCurahHujan);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [tekananUdaraData, setTekananUdaraData] = useState(0);
+  const generateRandomTekananUdara = () => {
+    return (Math.random() * (1005 - 1003) + 1003).toFixed(1);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newTekananUdara = generateRandomTekananUdara();
+      setTekananUdaraData(newTekananUdara);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [indexUVData, setIndexUVData] = useState(0);
+  const generateRandomIndexUVUdara = () => {
+    return (Math.random() * (7.0 - 5.5) + 5.5).toFixed(1);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newIndexUV = generateRandomIndexUVUdara();
+      setIndexUVData(newIndexUV);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [intensitasCahayaData, setIntensitasCahayaData] = useState(0);
+  const generateRandomIntensitasCahaya = () => {
+    return (Math.random() * (7.0 - 5.5) + 5.5).toFixed(1);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newIntensitasCahaya = generateRandomIntensitasCahaya();
+      setIntensitasCahayaData(newIntensitasCahaya);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [arahAnginData, setArahAnginData] = useState(0);
+  const generateRandomArahAngin = () => {
+    return Math.floor(Math.random() * (230 - 220) + 220);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newArahAngin = generateRandomArahAngin();
+      setArahAnginData(newArahAngin);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [hembusanAnginData, setHembusanAnginData] = useState(0);
+  const generateRandomHembusanAngin = () => {
+    return Math.floor(Math.random() * (230 - 220) + 220);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newHembusanAngin = generateRandomHembusanAngin();
+      setHembusanAnginData(newHembusanAngin);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [suhuData, setSuhuData] = useState(0);
+  const generateRandomSuhuData = () => {
+    return (Math.random() * (35 - 30) + 30).toFixed(1);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newSuhuData = generateRandomSuhuData();
+      setSuhuData(newSuhuData);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // const [socketData, setSocketData] = useState({});
+
+  // useEffect(() => {
+  //   socket.on("connect", () => {
+  //     console.log("Connected to Socket.IO server");
+  //   });
+
+  //   socket.on("sensor_data", (mess) => {
+  //     const dataArray = mess.split(",");
+  //     const data = {
+  //       distance:       parseFloat(dataArray[0]),
+  //       lightIntensity: parseFloat(dataArray[1]),
+  //       uvIndex:        parseFloat(dataArray[2]),
+  //       temperature:    parseFloat(dataArray[3]),
+  //       humidity:       parseFloat(dataArray[4]),
+  //       windSpeed:      parseFloat(dataArray[5]),
+  //       gustSpeed:      parseFloat(dataArray[6]),
+  //       windDirection:  parseFloat(dataArray[7]),
+  //       rainfall:       parseFloat(dataArray[8]),
+  //       absPressure:    parseFloat(dataArray[9]),
+  //     }
       
-      console.log(data);
+  //     console.log(data);
 
-      setSocketData({...data})
-    });
-  })
+  //     setSocketData({...data})
+  //   });
+  // })
+
+  // useEffect(() => {
+  //   socket.on("connect", () => {
+  //     console.log("Connected to Socket.IO server");
+  //   });
+  
+  //   socket.on("sensor_data", (mess) => {
+  //     // console.log("Raw sensor data received:", mess); // Log raw data
+  //     const dataArray = mess.split(",");
+  //     const data = {
+  //       distance: parseFloat(dataArray[0]),
+  //       lightIntensity: parseFloat(dataArray[1]),
+  //       uvIndex: parseFloat(dataArray[2]),
+  //       temperature: parseFloat(dataArray[3]),
+  //       humidity: parseFloat(dataArray[4]),
+  //       windSpeed: parseFloat(dataArray[5]),
+  //       gustSpeed: parseFloat(dataArray[6]),
+  //       windDirection: parseFloat(dataArray[7]),
+  //       rainfall: parseFloat(dataArray[8]),
+  //       absPressure: parseFloat(dataArray[9]),
+  //     };
+  
+  //     setSocketData(data);
+  //   });
+  
+  //   return () => {
+  //     socket.off("sensor_data");
+  //     socket.off("connect");
+  //   };
+  // }, []);
 
   return (
     <div className="h-screen p-4 bg-gray-200">
@@ -129,21 +227,21 @@ const Dashboard = () => {
           <Card className="flex flex-col">
             <div className="font-semibold text-sm">Kecepatan Angin</div>
             <div className="h-26 mt-3 rounded-md">
-              <KecepatanAnginGraph id="windSpeed" kecepatanAnginData={socketData.windSpeed} />
+              <KecepatanAnginGraph id="windSpeed" kecepatanAnginData={kecepatanAnginData} />
             </div>
           </Card>
 
           <Card className="flex flex-col">
             <div className="font-semibold text-sm">Ketinggian Air</div>
             <div className="h-26 mt-3 rounded-md">
-              <KetinggianAirGraph id="distance" ketinggianAirData={socketData.distance} />
+              <KetinggianAirGraph id="distance" ketinggianAirData={ketinggianData} />
             </div>
           </Card>
 
           <Card className="flex flex-col">
             <div className="font-semibold text-sm">Kelembaban</div>
             <div className="h-26 mt-3 rounded-md">
-              <KelembabanGraph id="humidity" kelembabanData={socketData.humidity} /> {/* Pass the random yawData */}
+              <KelembabanGraph id="humidity" kelembabanData={kelembabanData} />
             </div>
           </Card>
         </div>
@@ -160,11 +258,11 @@ const Dashboard = () => {
             </Card>
 
             <Card className="p-4 font-semibold text-sm">
-              Indeks UV = {socketData.uvIndex}
+              Indeks UV = {indexUVData}
             </Card>
 
             <Card className="p-4 font-semibold text-sm">
-              Intensitas Cahaya = {socketData.lightIntensity} lux
+              Intensitas Cahaya = {intensitasCahayaData} lux
             </Card>
 
             <Card className="p-4 h-full">
@@ -178,31 +276,31 @@ const Dashboard = () => {
               <Card className="flex flex-col justify">
                 <div className="font-semibold text-sm">Temperatur</div>
                 <div className="font-semibold text-sm">‎</div>
-                <div className="font-semibold text-4xl text-center align-baseline">{socketData.temperature}° C</div>
+                <div className="font-semibold text-4xl text-center align-baseline">{suhuData}° C</div>
               </Card>
 
               <Card className="flex flex-col justify">
                 <div className="font-semibold text-sm">Tekanan Udara</div>
                 <div className="font-semibold text-sm">‎ </div>
-                <div className="font-semibold text-4xl text-center align-baseline">{socketData.absPressure} mb</div>
+                <div className="font-semibold text-4xl text-center align-baseline">{tekananUdaraData}</div>
               </Card>
 
               <Card className="flex flex-col justify">
                 <div className="font-semibold text-sm">Arah Angin</div>
                 <div className="font-semibold text-sm">‎ </div>
-                <div className="font-semibold text-4xl text-center align-baseline">{socketData.windDirection}°</div>
+                <div className="font-semibold text-4xl text-center align-baseline">{arahAnginData}°</div>
               </Card>
 
               <Card className="flex flex-col justify">
                 <div className="font-semibold text-sm">Hembusan Angin</div>
                 <div className="font-semibold text-sm">‎ </div>
-                <div className="font-semibold text-4xl text-center align-baseline">{socketData.gustSpeed}</div>
+                <div className="font-semibold text-4xl text-center align-baseline">{hembusanAnginData}</div>
               </Card>
             </div>
             <Card className="flex flex-col justify h-full">
               <div className="font-semibold text-sm">Presipitasi</div>
               <div className="h-26 mt-3 rounded-md">
-                <CurahHujanGraph id="rainfall" curahHujanData={socketData.rainfall}/>
+                <CurahHujanGraph id="rainfall" curahHujanData={curahHujanData}/>
               </div>
             </Card>
           </div>
